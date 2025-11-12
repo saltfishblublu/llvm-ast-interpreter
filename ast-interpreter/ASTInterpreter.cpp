003adb4b24f535c068e7f7ab790766ad26b9461d
saltfishblublu <saltfishblublu@163.com>
2025-11-12 09:43:15 +0800

    Update ASTInterpreter.cpp

--- ast-interpreter/ASTInterpreter.cpp
+++ ast-interpreter/ASTInterpreter.cpp
@@ -11,66 +11,234 @@ using namespace clang;
 
 #include "Environment.h"
 
-class InterpreterVisitor : 
-   public EvaluatedExprVisitor<InterpreterVisitor> {
-public:
-   explicit InterpreterVisitor(const ASTContext &context, Environment * env)
-   : EvaluatedExprVisitor(context), mEnv(env) {}
-   virtual ~InterpreterVisitor() {}
-
-   virtual void VisitBinaryOperator (BinaryOperator * bop) {
-	   VisitStmt(bop);
-	   mEnv->binop(bop);
-   }
-   virtual void VisitDeclRefExpr(DeclRefExpr * expr) {
-	   VisitStmt(expr);
-	   mEnv->declref(expr);
-   }
-   virtual void VisitCastExpr(CastExpr * expr) {
-	   VisitStmt(expr);
-	   mEnv->cast(expr);
-   }
-   virtual void VisitCallExpr(CallExpr * call) {
-	   VisitStmt(call);
-	   mEnv->call(call);
-   }
-   virtual void VisitDeclStmt(DeclStmt * declstmt) {
-	   mEnv->decl(declstmt);
-   }
+/**
+ * AST Visitor that traverses and interprets the abstract syntax tree
+ */
+class ASTInterpreterVisitor : public EvaluatedExprVisitor<ASTInterpreterVisitor>
+{
 private:
-   Environment * mEnv;
-};
+    ExecutionEnvironment *executionEnv;
 
-class InterpreterConsumer : public ASTConsumer {
+    /**
+     * Checks if execution should be skipped due to pending return
+     */
+    bool shouldSkipExecution()
+    {
+        return executionEnv->callStack.back().hasPendingReturn();
+    }
+
+    /**
+     * Checks if function is user-defined (not built-in)
+     */
+    bool isUserDefinedFunction(FunctionDecl *funcDecl)
+    {
+        return (funcDecl->getName() != "GET") &&
+               (funcDecl->getName() != "PRINT") &&
+               (funcDecl->getName() != "MALLOC") &&
+               (funcDecl->getName() != "FREE");
+    }
+
 public:
-   explicit InterpreterConsumer(const ASTContext& context) : mEnv(),
-   	   mVisitor(context, &mEnv) {
-   }
-   virtual ~InterpreterConsumer() {}
+    explicit ASTInterpreterVisitor(const ASTContext &context, ExecutionEnvironment *env)
+        : EvaluatedExprVisitor(context), executionEnv(env) {}
+        
+    virtual ~ASTInterpreterVisitor() {}
 
-   virtual void HandleTranslationUnit(clang::ASTContext &Context) {
-	   TranslationUnitDecl * decl = Context.getTranslationUnitDecl();
-	   mEnv.init(decl);
+    /**
+     * Visits binary operator expressions
+     */
+    virtual void VisitBinaryOperator(BinaryOperator *binaryOp)
+    {
+        if (shouldSkipExecution()) return;
+        
+        VisitStmt(binaryOp);
+        executionEnv->evaluateBinaryOperation(binaryOp);
+    }
 
-	   FunctionDecl * entry = mEnv.getEntry();
-	   mVisitor.VisitStmt(entry->getBody());
-  }
+    /**
+     * Visits declaration reference expressions
+     */
+    virtual void VisitDeclRefExpr(DeclRefExpr *declRef)
+    {
+        if (shouldSkipExecution()) return;
+        
+        VisitStmt(declRef);
+        executionEnv->processDeclRefExpr(declRef);
+    }
+
+    /**
+     * Visits function call expressions
+     */
+    virtual void VisitCallExpr(CallExpr *callExpr)
+    {
+        if (shouldSkipExecution()) return;
+        
+        VisitStmt(callExpr);
+        executionEnv->processFunctionCall(callExpr);
+        
+        // Handle user-defined function returns
+        if (FunctionDecl *callee = callExpr->getDirectCallee())
+        {
+            if (isUserDefinedFunction(callee))
+            {
+                // Visit function body
+                Visit(callee->getBody());
+                
+                // Retrieve return value and clean up stack frame
+                int64_t returnValue = executionEnv->callStack.back().getReturnValue();
+                executionEnv->callStack.pop_back();
+                executionEnv->callStack.back().storeExpressionResult(callExpr, returnValue);
+            }
+        }
+    }
+
+    /**
+     * Visits declaration statements
+     */
+    virtual void VisitDeclStmt(DeclStmt *declStmt)
+    {
+        if (shouldSkipExecution()) return;
+        
+        VisitStmt(declStmt);
+        executionEnv->processDeclaration(declStmt);
+    }
+
+    /**
+     * Visits if statements
+     */
+    virtual void VisitIfStmt(IfStmt *ifStmt)
+    {
+        if (shouldSkipExecution()) return;
+        
+        Expr *condition = ifStmt->getCond();
+        if (executionEnv->evaluateExpression(condition))
+        {
+            // Execute then branch
+            Stmt *thenBranch = ifStmt->getThen();
+            Visit(thenBranch);
+        }
+        else if (ifStmt->getElse())
+        {
+            // Execute else branch
+            Stmt *elseBranch = ifStmt->getElse();
+            Visit(elseBranch);
+        }
+    }
+
+    /**
+     * Visits while statements
+     */
+    virtual void VisitWhileStmt(WhileStmt *whileStmt)
+    {
+        if (shouldSkipExecution()) return;
+        
+        Expr *condition = whileStmt->getCond();
+        while (executionEnv->evaluateExpression(condition))
+        {
+            Visit(whileStmt->getBody());
+        }
+    }
+
+    /**
+     * Visits for statements
+     */
+    virtual void VisitForStmt(ForStmt *forStmt)
+    {
+        if (shouldSkipExecution()) return;
+        
+        // Execute initialization
+        Stmt *initialization = forStmt->getInit();
+        if (initialization)
+        {
+            Visit(initialization);
+        }
+        
+        // Execute loop
+        for (; executionEnv->evaluateExpression(forStmt->getCond()); 
+             Visit(forStmt->getInc()))
+        {
+            Visit(forStmt->getBody());
+        }
+    }
+
+    /**
+     * Visits return statements
+     */
+    virtual void VisitReturnStmt(ReturnStmt *returnStmt)
+    {
+        if (shouldSkipExecution()) return;
+        
+        Visit(returnStmt->getRetValue());
+        executionEnv->processReturn(returnStmt);
+    }
+
+    /**
+     * Visits unary operator expressions
+     */
+    virtual void VisitUnaryOperator(UnaryOperator *unaryOp)
+    {
+        if (shouldSkipExecution()) return;
+        
+        VisitStmt(unaryOp);
+        executionEnv->evaluateUnaryOperation(unaryOp);
+    }
+};
+
+/**
+ * AST Consumer that handles the entire translation unit
+ */
+class InterpreterConsumer : public ASTConsumer
+{
 private:
-   Environment mEnv;
-   InterpreterVisitor mVisitor;
+    ExecutionEnvironment executionEnv;
+    ASTInterpreterVisitor astVisitor;
+
+public:
+    explicit InterpreterConsumer(const ASTContext &context) 
+        : executionEnv(), astVisitor(context, &executionEnv) {}
+        
+    virtual ~InterpreterConsumer() {}
+
+    /**
+     * Handles the complete translation unit
+     */
+    virtual void HandleTranslationUnit(clang::ASTContext &context)
+    {
+        TranslationUnitDecl *translationUnit = context.getTranslationUnitDecl();
+        executionEnv.initialize(translationUnit);
+
+        // Execute main function
+        FunctionDecl *mainFunction = executionEnv.getEntryFunction();
+        if (mainFunction) {
+            astVisitor.VisitStmt(mainFunction->getBody());
+        }
+    }
 };
 
-class InterpreterClassAction : public ASTFrontendAction {
-public: 
-  virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
-    clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
-    return std::unique_ptr<clang::ASTConsumer>(
-        new InterpreterConsumer(Compiler.getASTContext()));
-  }
+/**
+ * Frontend action that creates the interpreter consumer
+ */
+class InterpreterFrontendAction : public ASTFrontendAction
+{
+public:
+    virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+        clang::CompilerInstance &compiler, llvm::StringRef inputFile)
+    {
+        return std::unique_ptr<clang::ASTConsumer>(
+            new InterpreterConsumer(compiler.getASTContext()));
+    }
 };
 
-int main (int argc, char ** argv) {
-   if (argc > 1) {
-       clang::tooling::runToolOnCode(std::unique_ptr<clang::FrontendAction>(new InterpreterClassAction), argv[1]);
-   }
+/**
+ * Main entry point for the AST interpreter
+ */
+int main(int argc, char **argv)
+{
+    if (argc > 1)
+    {
+        clang::tooling::runToolOnCode(
+            std::make_unique<InterpreterFrontendAction>(), argv[1], "input.c");
+    }
+    
+    return 0;
 }
